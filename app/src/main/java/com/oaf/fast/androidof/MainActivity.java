34bb822c7cc77c5f3c6d63170911c916898e7b9f
liukun <liukun612@gmail.com>
2016-03-18 16:11:37 +0800

    修改包名

---
+++ app/src/main/java/com/oaf/fast/androidof/MainActivity.java
@@ -0,0 +1,19 @@
+package com.oaf.fast.androidof;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+
+/**
+ * Created by liukun on 16/3/18.
+ */
+public class MainActivity extends FragmentActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mian);
+    }
+}
