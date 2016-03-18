0a1d1681dbefa6f489b80600de12a3d657534cc4
liukun <liukun612@gmail.com>
2016-03-18 15:55:03 +0800

    Activity测试

---
+++ app/src/main/java/fast/oaf/com/androidof/MainActivity.java
@@ -0,0 +1,17 @@
+package fast.oaf.com.androidof;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by liukun on 16/3/18.
+ */
+public class MainActivity extends FragmentActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mian);
+    }
+}
